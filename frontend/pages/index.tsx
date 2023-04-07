e6666e2ae72c7a890273bf80f4259029929e2d98
Bryan Rodriguez <Serphp@hotmail.com>
2023-04-07 13:55:21 -0500

    New feature: Search Topic & small improvements scss

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,9 @@
+import React, { useState } from 'react';
 import Link from 'next/link';
+import moment from 'moment';
 import groq from 'groq';
 import { client } from '../lib/client';
-import React from 'react';
-//import { PostContext } from './components/Context/PostContext';
 const PortableText = require('@portabletext/react').PortableText;
-import moment from 'moment';
 
 interface Post {
   body: string;
@@ -20,47 +19,77 @@ interface PostsProps {
   categories: { title: string }[];
 }
 
-const Home = ({ posts, categories  }: PostsProps) => {
-  //const { handleTheme } = useContext(PostContext);
-  //console.log(postsAndCategories);
-  //console.log(posts)
-  
+const Home = ({ posts, categories }: PostsProps) => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const filteredPosts = posts.filter((post) => {
+    return post.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <>
+      <section className="py-l5">
+        <h1 className="paragraph">Bienvenido a mis pensamientos</h1>
+        <p className="paragraph2">
+          Aquí encontrarás mis pensamientos, reflexiones, y experiencias.
+        </p>
 
-    <section className="py-l5">
+        <div className='contenedor'>
+          <input
+            type="text"
+            className="imput"
+            value={searchTerm}
+            onChange={handleSearch}
+            placeholder="¿Que buscas hoy?"
+          />
+        </div>
 
-      <h1 className='paragraph'> Bienvenido a mis pensamientos </h1>
-      <p className='paragraph2'> Aquí encontrarás mis pensamientos, reflexiones, y experiencias. </p>
 
         <div className="flex flex-column md-flex-row md-w-90pc mx-auto contenedorcard">
-
-          {posts.map((post) => (
-            <div className="w-100pc md-w-50pc" key={post._id}>
+          {filteredPosts.length > 0 ? (
+            filteredPosts.map((post) => (
+              <div className="w-100pc md-w-50pc" key={post._id}>
                 <div className="card2 pointer">
-                      <div className="inline-block bg-indigo-lightest-30 indigo-lightest br-3 px-4 py-1 mb-10 fs-s4 uppercase ">
-                      {moment(post.publishedAt).fromNow()}
-                      </div>
-                    <div className="indigo-lightest fw-400 fs-m1">{post.title} 
-                      <span className="opacity-30"> 
-                      <div>  {post.categories} </div>
-                      </span> 
-                    <p className="opacity-50"> 
-                      <PortableText x={post.body && post.body.toString().slice(0, 20)}/>
+                  <div className="inline-block bg-indigo-lightest-30 indigo-lightest br-3 px-4 py-1 mb-10 fs-s4 uppercase ">
+                    {moment(post.publishedAt).fromNow()}
+                  </div>
+                  <div className="indigo-lightest fw-400 fs-m1">
+                    {post.title}
+                    <span className="opacity-30">
+                      <div> {post.categories} </div>
+                    </span>
+                    <p className="opacity-50">
+                      <PortableText
+                        x={
+                          post.body &&
+                          post.body.toString().slice(0, 20)
+                        }
+                      />
                     </p>
-                    </div>
-                      <Link href="/post/[slug]" as={`/post/${post.slug}`} className="mt-10 button bg-indigo-lightest-20 bg-white fs-s3 black no-underline">
-                      Read
-                      </Link>
+                  </div>
+                  <Link
+                    href="/post/[slug]"
+                    as={`/post/${post.slug}`}
+                    className="mt-10 button bg-indigo-lightest-20 bg-white fs-s3 black no-underline"
+                  >
+                    Read
+                  </Link>
                 </div>
+              </div>
+            ))
+          ) : (
+            <div className='flex justify-center items-center'>
+            <p className='card3'>No hay resultados que coincidan con tu búsqueda</p>
             </div>
-          ))}
+          )}
         </div>
-    </section>
-        </>
+      </section>
+    </>
   );
 };
-
 
 export async function getStaticProps() {
   const postsAndCategories = await client.fetch(groq`
@@ -84,7 +113,5 @@ export async function getStaticProps() {
     },
   };
 }
-
-
 
 export default Home;
