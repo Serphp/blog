7012ea79aae5804b016954129e5aa9ed47d7dded
Bryan Rodriguez <Serphp@hotmail.com>
2023-04-06 23:25:08 -0500

    New Update: Index & footer & Navbar & new styles

--- frontend/pages/components/main/Navbar.tsx
+++ frontend/pages/components/main/Navbar.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/alt-text */
 import Link from "next/link";
 import { useContext } from 'react';
 import { PostContext } from '../Context/PostContext';
+import Social from "./Socials";
 
 export default function Navbar() {
 
@@ -9,22 +11,34 @@ export default function Navbar() {
     //console.log(toggleTheme);
 
 return (
-<nav>
-    <div className="theme">
-        <Link href="/">
-            <span className="nav-text"> Home </span>
-        </Link>
-        <Link href="/about">
-            <span className="nav-text"> About </span>
-        </Link>
-        <Link href="/categories">
-            <span className="nav-text"> Categories </span>
-        </Link>
+<nav className="flex flex-column md-flex-row px-10">
+    <div className="flex justify-between">
+        <span className="flex items-center p-10 mr-5">
+            <img className="max-h-l3 w-auto hover-scale-up-1 ease-300" src="https://i.imgur.com/pimVkwe.png"/>
+            Hola
+            </span>
+        <a data-toggle="toggle-nav" data-target="#nav-items" href="#"
+            className="flex items-center ml-auto md-hidden opacity-70">
+            <div className="m-3 br-4 border-white p-1">
+                <div className="border-t border-white my-1 px-3"></div>
+                <div className="border-t border-white my-1"></div>
+                <div className="border-t border-white my-1"></div>
+            </div>
+        </a>
     </div>
-
-    <button className='toggletheme' onClick={handleTheme}>
-    hola 
+    <div id="nav-items" className="hidden flex sm-w-100pc flex-column md-flex md-flex-row md-justify-start items-center">
+        <a href="/" className="fs-s1 mx-3 py-3 white no-underline hover-underline">Home</a>
+        <a href="/about" className="fs-s1 mx-3 py-3 white no-underline hover-underline">About</a>
+        <a href="/categories" className="fs-s1 mx-3 py-3 white no-underline hover-underline">Pricing</a>
+    </div>
+    <span className="flex items-center p-2 mr-4">
+        <Social/>
+    </span>
+    <span className="flex items-center p-2 mr-4">
+    <button className='button bg-blue-lightest blue focus-blue' onClick={handleTheme}>
+    Color 
     </button>
-
-    </nav>)
+            </span>
+</nav>
+    )
 }
